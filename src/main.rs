31e8d06bac7198fdf946e15b36e5df27c07cb35b
Haraldur Björn Sigþórsson <hb150794@gmail.com>
2020-08-10 11:48:50 +0000

    comments and refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-use std::collections::VecDeque;
-
 //---------------CELL---------------
 
 struct Cell {
@@ -21,14 +19,17 @@ impl Cell {
         }
     }
 
+    // has this number been tried before in this cell?
     fn is_number_excluded(self: &Self, value: i32) -> bool{
         self.excluded_numbers.contains(&value)
     }
 
+    // the current number is probably blocking the solution
     fn add_to_excluded_numbers(self: &mut Self){
         self.excluded_numbers.push(self.value);
     }
 
+    //the problem is not this cell, clear the tried numbers.
     fn clear_excluded_numbers(self: &mut Self){
         self.excluded_numbers.clear();
     }
@@ -37,6 +38,7 @@ impl Cell {
 
 fn main() {
 
+    // the original puzzle
     let init_board: Vec<i32> = vec![
         5,3,0,  0,7,0,  0,0,0,
         6,0,0,  1,9,5,  0,0,0,
@@ -51,6 +53,7 @@ fn main() {
         0,0,0,  0,8,0,  0,7,9
         ];
     
+    //points to the corresponding row (litarally just the index...)
     let row_indicies: Vec<usize> = vec![
         0, 1, 2, 3, 4, 5, 6, 7, 8,
         9,10,11,12,13,14,15,16,17,
@@ -62,7 +65,8 @@ fn main() {
         63,64,65,66,67,68,69,70,71,
         72,73,74,75,76,77,78,79,80
     ];
-
+    
+    // points to the corresponding columns (read top > down)
     let column_indicies: Vec<usize> = vec![
         0, 9,18,27,36,45,54,63,72,
         1,10,19,28,37,46,55,64,73,
@@ -75,6 +79,7 @@ fn main() {
         8,17,26,35,44,53,62,71,80
     ];
 
+    // points to the corresponding box
     let box_indicies: Vec<usize> = vec![
         0, 1, 2, 9,10,11,18,19,20,
         3, 4, 5,12,13,14,21,22,23,
@@ -96,34 +101,40 @@ fn main() {
     while current_index < 80 {
         // is this cell predetermined
         if init_board[current_index] != 0 {
-            //println!("Value at {} is immutable", current_index);
             //go to the next cell
             current_index += 1;
-        } else {
-            //check for every value ABOVE the current value
+        } 
+        else {
+            //check for every value 
             for val in 1..=9 {
+                // is the value unique AND hasn't been tried before?
                 if is_valid_and_unique((&row_indicies, &column_indicies, &box_indicies), current_index, val, &board){
+                    // if it is, assign this cell this value and go to the next cell
                     board[current_index].value = val;
-                    //print_board(&board);
                     current_index += 1;
                     break;
                 }
                 else if val == 9 {
+                    //if we made it here, no value fits this cell
+                    //reset the values tried in this cell
                     board[current_index].clear_excluded_numbers();
+                    // go to the previous cell
                     current_index -= 1;
+                    // find the previous editable cell
                     while init_board[current_index] != 0 {
                         if current_index == 0 {
+                            //if there is no editable cell, the puzzle in unsolvable
                             println!("Puzzle unsolvable :(");
                             return;
                         }
                         current_index -= 1;
                     }
+                    //if we found an editable cell, add it's current value to the list of already tried values.
                     board[current_index].add_to_excluded_numbers();
                     board[current_index].value = 0;
                 }    
             }
         }
-        //print_loading_bar(current_index);
     }
     println!("Solved!");
     print_board(&board);
@@ -131,25 +142,28 @@ fn main() {
 
 //------------FUNCTIONS-------------
 
-fn read_cells_into_board(init_board: &[i32]) -> VecDeque<Cell>{
-    let mut board: VecDeque<Cell> = VecDeque::new();
+// creates a vector of cells from a vector of ints
+fn read_cells_into_board(init_board: &[i32]) -> Vec<Cell>{
+    let mut board: Vec<Cell> = Vec::new();
     for row in 0..9{
         for column in 0..9 {
             let i = row * 9 + column;
-            board.push_back(Cell::new(init_board[i], row, column, get_assigned_box(row, column)));
+            board.push(Cell::new(init_board[i], row, column, get_assigned_box(row, column)));
         }
     } 
     board
 }
 
-fn is_valid_and_unique((rows, columns, boxes): (&[usize], &[usize], &[usize]), current_index: usize, value: i32, board: &VecDeque<Cell>) -> bool {
+// checks if a value is unique in its row, column, box AND if it has been tried before.
+fn is_valid_and_unique((rows, columns, boxes): (&[usize], &[usize], &[usize]), current_index: usize, value: i32, board: &[Cell]) -> bool {
     !board[current_index].is_number_excluded(value) && 
             is_unique(board[current_index].row_number, &rows, value, current_index, &board) &&
             is_unique(board[current_index].column_number, &columns, value, current_index, &board) &&
             is_unique(board[current_index].box_number, &boxes, value, current_index, &board)
 }
 
-fn is_unique(container_index: usize, container: &[usize], value: i32, current_index: usize, board: &VecDeque<Cell>) -> bool {
+//checks if the value is unique in this specific row/column/box
+fn is_unique(container_index: usize, container: &[usize], value: i32, current_index: usize, board: &[Cell]) -> bool {
     let mut current_container: Vec<usize> = Vec::new();
     for i in 0..9{
         current_container.push(container[container_index * 9 + i]);
@@ -162,35 +176,18 @@ fn is_unique(container_index: usize, container: &[usize], value: i32, current_in
     true
 }
 
+// check what box this cell belongs to based on the row and column.
 fn get_assigned_box(row: usize, column: usize) -> usize {
     if row < 3 {
-        if column < 3 {
-            0
-        } else if column > 5 {
-            2
-        } else {
-            1
-        }
-    } else if row > 5 {
-        if column < 3 {
-            6
-        } else if column > 5 {
-            8
-        } else {
-            7
-        }
-    } else if column < 3 {
-        3
-    } else if column > 5 {
-        5
-    } else {
-        4
-    }
+        if column < 3 { 0 } else if column > 5 { 2 } else { 1 }
+    } 
+    else if row > 5 {
+        if column < 3 { 6 } else if column > 5 { 8 } else { 7 }
+    } 
+    else if column < 3 { 3 } else if column > 5 { 5 } else { 4 }
 }
 
-//-----------------------DEBUG------------------------
-
-fn print_board(board: &VecDeque<Cell>){
+fn print_board(board: &[Cell]){
     for i in 0..9 {
         for j in 0..9{
             if board[i*9+j].value == 0 {
